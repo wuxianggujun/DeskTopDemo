0c4860aaf0e39a417d15a6a6b891332b1689ce6c
无相孤君 <39469560+wuxianggujun@users.noreply.github.com>
2022-07-22 11:51:01 +0800

    Initial commit

---
+++ demo1/src/jvmMain/kotlin/Main.kt
@@ -0,0 +1,56 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+@Composable
+@Preview
+fun App() {
+    var text by remember { mutableStateOf("Hello, World!") }
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        val names = mutableListOf("1","2","3","4","5")
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+
+}
+@Composable
+private fun Greeting(name: String) {
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f).padding(bottom = extraPadding)) {
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+            OutlinedButton(
+                onClick = { expanded.value = !expanded.value }
+            ) {
+                Text(if (expanded.value) "收缩" else "展开")
+            }
+        }
+    }
+}
+
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication, title = "Compose for Desktop") {
+        App()
+    }
+}
